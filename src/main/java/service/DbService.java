aa927094f294f2ddd353345c71265ba8f6f1fdec
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-07-10 09:22:59 -0700

    database

--- src/main/java/service/DbService.java
+++ src/main/java/service/DbService.java
@@ -8,10 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DbService {
-    private static final String dbName = "web-4-first-spring";
-    private static final String url = "jdbc:postgresql://localhost:5432/" + dbName;
-    private static final String username = "postgres";
-    private static final String password = "root123";
+    //    private static final String dbName = "web-4-first-spring";
+//    private static final String url = "jdbc:postgresql://localhost:5432/" + dbName;
+//    private static final String username = "postgres";
+//    private static final String password = "root123";
+    private static final String dbName = "dbm7kcj6rkrdf2";
+    private static final String url = "postgres://pxqdzszbkhztpm:f54d80f803d46b6d9b828453f7c9f5704d0091d47bfec26bacdc06f64023be31@ec2-3-234-126-10.compute-1.amazonaws.com:5432/dbm7kcj6rkrdf2";
+    private static final String username = "pxqdzszbkhztpm";
+    private static final String password = "f54d80f803d46b6d9b828453f7c9f5704d0091d47bfec26bacdc06f64023be31";
 
     public List<Phone> getPhoneList() throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
